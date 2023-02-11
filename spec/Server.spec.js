227a92ae9dfd225fd6d5bb1aa419ec8db7225b96
Yuriy Pliskovsky <zzaafamania@gmail.com>
2023-02-11 13:53:26 +0300

    Add files via upload

--- spec/Server.spec.js
+++ spec/Server.spec.js
@@ -1,15 +1,16 @@
 const fs = require('fs').promises;
 const path = require('path');
 const os = require('os');
+const crypto = require('crypto');
 const Server = require('../Server');
 const Container = require('../Container');
 
 describe('Класс Server', () => {
-  const containerInstanceNode = new Container({ baseImage: 'node', architecture: 'x86-64', version: 18.14 });
-  const containerInstancePostges = new Container({ baseImage: 'postgres', architecture: 'x86-64', version: 15.1 });
-  const containerInstancePyhton = new Container({ baseImage: 'python', architecture: 'x86-64', version: 3.9 });
-  const containerInstanceGo = new Container({ baseImage: 'go', architecture: 'x86-64', version: 1.20 });
-  const containerInstanceMongo = new Container({ baseImage: 'mongo', architecture: 'x86-64', version: 6.0 });
+  const containerInstanceNode = new Container({ baseImage: 'node', architecture: 'x86-64', version: 18.14, id: crypto.randomUUID() });
+  const containerInstancePostges = new Container({ baseImage: 'postgres', architecture: 'x86-64', version: 15.1, id: crypto.randomUUID() });
+  const containerInstancePyhton = new Container({ baseImage: 'python', architecture: 'x86-64', version: 3.9, id: crypto.randomUUID() });
+  const containerInstanceGo = new Container({ baseImage: 'go', architecture: 'x86-64', version: 1.20, id: crypto.randomUUID() });
+  const containerInstanceMongo = new Container({ baseImage: 'mongo', architecture: 'x86-64', version: 6.0, id: crypto.randomUUID() });
 
   const serverInstance = new Server({
     status: 'offline',
@@ -36,10 +37,7 @@ describe('Класс Server', () => {
   });
 
   describe('Остановка сервера', () => {
-    beforeEach(() => {
-      serverInstance.stopServer();
-    });
-
+    serverInstance.stopServer();
 
     it('Функционал остановки сервера, меняется статус', () => {
       const status = 'offline';
@@ -48,33 +46,41 @@ describe('Класс Server', () => {
   });
 
   describe('Защита от переопределения статуса сервера вручную', () => {
-    serverInstance.startServer();
+    it('Метод запуска сервера', () => {
+      serverInstance.startServer();
 
-    serverInstance.status = 'offline';
-    expect(serverInstance.status).toBe('online');
+      serverInstance.status = 'offline';
+      expect(serverInstance.status).toBe('online');
+    })
   });
 
   describe('Добавление нового контейнера', () => {
-    serverInstance.addContainer(containerInstanceMongo);
+    it('Метод добавления контейнера', () => {
+      serverInstance.addContainer(containerInstanceMongo);
 
-    expect(serverInstance.allContainers).toEqual([
-      containerInstanceNode,
-      containerInstancePostges,
-      containerInstancePyhton,
-      containerInstanceGo,
-      containerInstanceMongo
-    ]);
+      expect(serverInstance.allContainers).toEqual([
+        containerInstanceNode,
+        containerInstancePostges,
+        containerInstancePyhton,
+        containerInstanceGo,
+        containerInstanceMongo
+      ]);
+    })
   });
 
   describe('Удаление контейнера', () => {
-    serverInstance.delContainer('python');
+    it('Метод удаления контейнера', () => {
+      const { id } = containerInstancePyhton;
 
-    expect(serverInstance.allContainers).toEqual([
-      containerInstanceNode,
-      containerInstancePostges,
-      containerInstanceGo,
-      containerInstanceMongo
-    ]);
+      serverInstance.delContainer(id);
+
+      expect(serverInstance.allContainers).toEqual([
+        containerInstanceNode,
+        containerInstancePostges,
+        containerInstanceGo,
+        containerInstanceMongo
+      ]);
+    })
   });
 
   describe('Логи сервера', () => {
@@ -95,7 +101,7 @@ describe('Класс Server', () => {
       const logs = await fs.readFile(logFolder, { encoding: 'utf-8' });
       const lastServerLog = logs.split(os.EOL).at(-2);
 
-      expect(lastServerLog).toBe('OS: ubuntu, CPU: amd, CORES: 8, CONTAINERS: [{"baseImage":"node","architecture":"x86-64"},{"baseImage":"postgres","architecture":"x86-64"},{"baseImage":"go","architecture":"x86-64"},{"baseImage":"mongo","architecture":"x86-64"}]');
+      expect(lastServerLog).toBe(`OS: ${serverInstance.os}, CPU: ${serverInstance.cpu}, CORES: ${serverInstance.cores}, CONTAINERS: ${JSON.stringify(serverInstance.allContainers)}`);
     });
   });
 
