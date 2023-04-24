d5f715ebddc6e65432f06d2f652886c31a9ecd5b
grentank <mexmat100394@gmail.com>
2023-04-24 22:07:53 +0300

    fixes done

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,12 @@ module.exports = {
     commonjs: true,
     es2021: true,
     node: true,
+    jest: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
-  plugins: [
-    'jest',
-  ],
+  extends: ['airbnb-base'],
+  plugins: ['jest'],
   parserOptions: {
     ecmaVersion: 'latest',
   },
-  rules: {
-  },
+  rules: {},
 };
